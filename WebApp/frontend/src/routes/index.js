b2862c826f62c417a8d6b714fbb219ae31055f58
vuong2404 <minhvuonglht@gmail.com>
2023-03-03 11:17:14 +0700

    update folder structure

---
+++ WebApp/frontend/src/routes/index.js
@@ -0,0 +1,8 @@
+import Dashboard from "../Pages/Dashborad"
+import MainLayout from "../Layouts/MainLayout"
+
+export const routes = [
+    {
+        path: '/' , component: Dashboard, layout: MainLayout
+    },
+]
--- web-app/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Dashboard from "../Pages/Dashboard"
-import MainLayout from "../Layouts/MainLayout"
-
-export const routes = [
-    {
-        path: '/dashboard', component: Dashboard, layout: MainLayout
-    },
-]
