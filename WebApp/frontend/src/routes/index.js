1c42a9c442bd94c4ab656209f78aaba05fde7c41
NguyenHung <nguyenhung.220102@gmail.com>
2023-03-11 14:09:10 +0700

    Merge change from master

--- WebApp/frontend/src/routes/index.js
+++ WebApp/frontend/src/routes/index.js
@@ -1,28 +1,23 @@
 import Dashboard from "../Pages/Dashborad"
 import MainLayout from "../Layouts/MainLayout"
-<<<<<<< HEAD
 import WaterPlan from "../Pages/Plan/WaterPlan"
 import LightPlan from "../Pages/Plan/LightPlan"
-=======
 import Home from "../Pages/Home"
 import Login from "../Pages/Login"
 import LoginLayout from "../Layouts/LoginLayout"
 import Register from "../Pages/Register"
 
->>>>>>> aff33cdd7fefc04d28e3436343d6986d0881f04a
 export const routes = [
     {
         path: '/' , component: Home, layout: null
     },
     {
-<<<<<<< HEAD
         path: '/waterplan' , component: WaterPlan, layout: MainLayout
     },
     {
         path: '/lightplan' , component: LightPlan, layout: MainLayout
-    }
-
-=======
+    },
+    {
         path: '/Login' , component:Login, layout: LoginLayout
     },
     {
@@ -31,5 +26,4 @@ export const routes = [
     {
         path: '/Dashboard' , component: Dashboard, layout: MainLayout
     }
->>>>>>> aff33cdd7fefc04d28e3436343d6986d0881f04a
 ]
